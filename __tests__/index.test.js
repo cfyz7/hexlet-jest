82a6902dfd9dab5c5be495f41f511326e7c08f27
cfyz7 <lu5294677@gmail.com>
2022-11-22 10:22:45 +0600

    add workflows

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,15 @@
 import reverse from '../src/index.js';
+import _ from 'lodash';
 
 test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh');
   expect(reverse('')).toEqual('');
+  expect(reverse('world')).toEqual('dlrow');
+});
+
+const coll = ['One', true, 3, 10, 'cat', {}, '', 10, false]; 
+
+test('includes', () => {
+  expect(_.includes(coll, 3)).toBe(true);
+  expect(_.includes(coll, 11)).toBe(false);
 });
